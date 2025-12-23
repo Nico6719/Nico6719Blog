31fd2768bb4cab0fab52b3c341daa85f93f8451d
Nico6719 <opqstu1333@outlook.com>
2025-12-24 01:35:50 +0800

    Update index.ts

--- www/.vitepress/theme/index.ts
+++ www/.vitepress/theme/index.ts
@@ -1,10 +1,15 @@
-import DefaultTheme from 'vitepress/theme'
+// 1. 尝试修改默认主题的导入路径
+import Theme from 'vitepress/theme'
 import TeekTheme from 'vitepress-theme-teek'
 
-// 如果构建还报样式错误，请尝试注释掉下面这一行
-// import 'vitepress-theme-teek/style.css' 
+// 暂时先不要引入 style.css，等构建通过了再说
+// import 'vitepress-theme-teek/style.css'
 
 export default {
-  extends: DefaultTheme,
-  ...TeekTheme
+  ...Theme,      // 使用解构直接覆盖
+  ...TeekTheme,
+  enhanceApp({ app, router, siteData }) {
+    // 如果主题有其他的插件注册，可以在这里写
+    TeekTheme.enhanceApp?.({ app, router, siteData })
+  }
 }
