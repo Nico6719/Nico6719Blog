db448707230996e248c143f2ecb41d8466d153f4
Nico6719 <opqstu1333@outlook.com>
2025-12-24 01:42:43 +0800

    Update index.ts

--- www/.vitepress/theme/index.ts
+++ www/.vitepress/theme/index.ts
@@ -1,16 +1,5 @@
 import DefaultTheme from 'vitepress/theme'
-import TeekTheme from 'vitepress-theme-teek'
-
-// 之前确认过这个包里没有 style.css，所以我们继续注释掉它
-// 如果你发现页面没样式，稍后我们用别的方法补救
-// import 'vitepress-theme-teek/style.css'
 
 export default {
-  ...DefaultTheme,
-  ...TeekTheme,
-  enhanceApp(ctx) {
-    // 调用默认主题和第三方主题的增强函数
-    DefaultTheme.enhanceApp?.(ctx)
-    TeekTheme.enhanceApp?.(ctx)
-  }
+  extends: DefaultTheme
 }
